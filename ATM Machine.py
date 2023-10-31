e735135dfe5dbd800b8775fdbc1c221f07f7eefa
KrishivDawra <117970322+KrishivDawra@users.noreply.github.com>
2023-10-31 21:49:59 +0530

    Create ATM Machine.py

---
+++ ATM Machine.py
@@ -0,0 +1,13 @@
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    s=""
+    for j in range(n):
+        if(a[j]<=k):
+            s=s+'1'
+            k=k-a[j]
+        else:
+            s=s+'0'
+    
+    print(s)
