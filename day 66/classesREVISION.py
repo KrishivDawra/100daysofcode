dfb8e2ecddc2e7a5d5dc0c77ae9f27d674d7e87b
Krishiv Dawra <117970322+KrishivDawra@users.noreply.github.com>
2023-12-20 22:23:40 +0530

    Create classesREVISION.py

---
+++ day 66/classesREVISION.py
@@ -0,0 +1,19 @@
+class person:
+    name="krishiv"
+    status="student"
+    age=17
+    def info(self):
+        print(self.name," is a ",self.status)
+
+a=person()
+b=person()
+c=person()
+
+a.name="Krish"
+a.status="developer"
+a.info()
+
+b.name="Dawra"
+b.status="MERN"
+b.info()
+c.info()
